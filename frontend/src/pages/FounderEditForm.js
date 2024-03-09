ca61ab1c1521ac2f0930d7d69acf404ad029040b
triviaspace <shafatriviaa@gmail.com>
2024-03-09 16:25:12 +0700

    benerin cookies

--- frontend/src/pages/FounderEditForm.js
+++ frontend/src/pages/FounderEditForm.js
@@ -44,7 +44,7 @@ const FounderDetails = () => {
               method: "GET", 
               headers:{
                   'Content-Type': 'application/json',
-                  'Authorization': 'Bearer ' + token
+                  'Authorization': 'Bearer' + token
               }
               }
               );
@@ -76,7 +76,7 @@ const FounderDetails = () => {
             method: "PUT",
             headers: {
                 "Content-Type": "application/json",
-                'Authorization': 'Bearer ' + token
+                'Authorization': 'Bearer' + token
             },
             body: JSON.stringify(founderDetails),
         });
