b6a484f67a71d7b88f1bc39aade323d63c3d49fd
Naznien Fevrianne Malano <111237114+naznienfevrianne@users.noreply.github.com>
2024-03-13 08:56:42 +0700

    try css admin

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,6 +2,8 @@
 from django.urls import path, include
 from django.contrib import admin
 from rest_framework import routers, serializers, viewsets
+from . import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -12,3 +14,5 @@ urlpatterns = [
     path('diary/', include('diaryentries.urls')),
     path('admin/', admin.site.urls)
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
