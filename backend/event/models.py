09d1bfbee268f7b28da89ee5c8b06f827ee0a329
Riona Adani Ghaisani <riona.adani@ui.ac.id>
2024-04-30 15:45:55 +0700

    create model event

--- backend/event/models.py
+++ backend/event/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from authentication.models import Partner
 
 # Create your models here.
+class Event (models.Model):
+    name = models.CharField(max_length = 255)
+    summary = models.CharField(max_length = 250)
+    desc = models.TextField(max_length=1000)
+    location = models.TextField()
+    date = models.DateField()
+    price = models.IntegerField()
+    link = models.TextField()
+    isVerified = models.IntegerField()
+    rejectionNote = models.CharField(max_length = 255, blank=True, default="")
+    partner = models.ForeignKey(Partner, related_name="partner", on_delete=models.CASCADE)
