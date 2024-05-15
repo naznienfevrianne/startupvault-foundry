53e30216339ae9bf3d18395a9e83ec146670020d
triviaspace <shafatriviaa@gmail.com>
2024-05-15 10:27:50 +0700

    bismillah startup di dashboard

--- frontend/src/pages/FounderDashboard.js
+++ frontend/src/pages/FounderDashboard.js
@@ -357,7 +357,7 @@ function FounderDashboard(props) {
                     overflow: 'hidden',
                     textOverflow: 'ellipsis',
                     whiteSpace: 'nowrap',  // Ensures the text stays in a single line
-                    maxWidth: '60%',      // Use maxWidth to allow the container to grow and shrink dynamically
+                    maxWidth: '70%',      // Use maxWidth to allow the container to grow and shrink dynamically
                   }}>
                     {startupData.name}
                   </div>
